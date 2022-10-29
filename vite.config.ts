c6ffcf4218486e94fb802b1f0aeae1e62041beae
Monyer <1435145+Monyer@users.noreply.github.com>
2022-10-29 15:44:45 +0800

    vite build config & github link

--- vite.config.ts
+++ vite.config.ts
@@ -10,20 +10,21 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
+    plugins: [
+        vue(),
+        vueJsx(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
+        },
     },
-  },
+    build: { outDir: "docs" },
 });
