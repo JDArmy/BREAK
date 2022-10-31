052cc0e8bd317aa3c477d5f5b2482014d506f340
Monyer <1435145+Monyer@users.noreply.github.com>
2022-10-31 11:06:17 +0800

    update.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,7 @@ const router = createRouter({
         {
             path: "/",
             name: "home",
-            component: () => import("@/views/HomeView.vue"),
+            component: HomeView,
         },
         {
             path: "/risks",
