b86a1746e17d4fcf5cea1521ee6edcc0824e9da5
Monyer <1435145+Monyer@users.noreply.github.com>
2022-11-03 18:37:43 +0800

    fix router to hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,41 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+// createWebHistory(import.meta.env.BASE_URL),
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        {
-            path: "/risk/:rKey",
-            name: "riskDetail",
-            component: HomeView,
-        },
-        {
-            path: "/risks",
-            name: "risks",
-            component: () => import("@/views/RisksView.vue"),
-        },
-        {
-            path: "/avoidances",
-            name: "avoidances",
-            component: () => import("@/views/AvoidancesView.vue"),
-        },
-        {
-            path: "/business-scenes",
-            name: "businessScenes",
-            component: () => import("@/views/BusinessScenesView.vue"),
-        },
-        {
-            path: "/ability-providers",
-            name: "abilityProviders",
-            component: () => import("@/views/AbilityProvidersView.vue"),
-        },
-    ],
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/risk/:rKey",
+      name: "riskDetail",
+      component: HomeView,
+    },
+    {
+      path: "/risks",
+      name: "risks",
+      component: () => import("@/views/RisksView.vue"),
+    },
+    {
+      path: "/avoidances",
+      name: "avoidances",
+      component: () => import("@/views/AvoidancesView.vue"),
+    },
+    {
+      path: "/business-scenes",
+      name: "businessScenes",
+      component: () => import("@/views/BusinessScenesView.vue"),
+    },
+    {
+      path: "/ability-providers",
+      name: "abilityProviders",
+      component: () => import("@/views/AbilityProvidersView.vue"),
+    },
+  ],
 });
 
 export default router;
