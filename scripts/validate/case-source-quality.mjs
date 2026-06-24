58c87da1fc28375b232ef4cfa0236aa640225abf
monyer <1435145+Monyer@users.noreply.github.com>
2026-06-25 03:36:58 +0800

    补强高价值案例 primary 来源

--- scripts/validate/case-source-quality.mjs
+++ scripts/validate/case-source-quality.mjs
@@ -304,6 +304,7 @@ const primaryReferenceLinks = new Set([
   'https://m.thepaper.cn/baijiahao_28782951', // 延津县人民法院官方澎湃号
   'https://www.thepaper.cn/newsdetail_forward_2921336', // 浙江法院系统政务稿，首例恶意注册账号案
   'https://www.sohu.com/a/1037162346_99923255', // 《方圆》杂志原创，徐州经开区检察院审查起诉
+  'https://web.archive.org/web/20220621092007/https://weibo.com/6329746106/LyKD2xUk8', // 学习通官方微博数据泄露传闻声明存档
   'https://view.inews.qq.com/a/20251205a03wsm00', // 抖音黑板报官方托管页，盗号黑产治理公告
   'https://mp.weixin.qq.com/s/wsro3i0l02fku9gr0o9gfw', // 公安部网安局官方微信，变脸软件解封涉诈账号
   'https://content-static.cctvnews.cctv.com/snow-book/index.html?item_id=16452910468894153646&track_id=06cab3b6-40f2-45f6-b2ee-15f8dc4ae04d', // 央视新闻客户端转述沁水公安 USDT 洗钱案
@@ -311,6 +312,7 @@ const primaryReferenceLinks = new Set([
   'https://mp.weixin.qq.com/s?__biz=mziwnzexnti5ma==&mid=2649895992&idx=1&sn=2db768454308aea8661283b04870a38d&chksm=8e40cffb5980f6350bf9e907cc441b820572068c2b4b5e6215a0f98ae38e4b465829b53ca1a4&scene=27', // 上海高院官方微信，临期食品恶意索赔案
   'https://www.zhongyuan.gov.cn/rdhy/9581840.jhtml', // 政府门户转载抖音黑板报官方治理公示
   'http://putian.pafj.net/caw/f/10/view-225-766722.html', // 莆田长安网检察院栏目，中共莆田市委政法委员会主办
+  'https://www.msxf.com/news/xwxq/1326', // 马上消费金融官网，反催收团伙协查公告
   'https://x.com/alvierid/status/1999403353466421320', // 安全研究人员原始披露
   'https://xz.aliyun.com/news/13288', // 作者原始漏洞复盘
   'https://xz.aliyun.com/news/14031', // 作者原始云存储接管复盘
