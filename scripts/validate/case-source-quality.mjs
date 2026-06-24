7e0116a6b1cad7cad8b9746260748d3cf6e1fec4
monyer <1435145+Monyer@users.noreply.github.com>
2026-06-24 22:29:10 +0800

    补强案例 primary 来源覆盖

--- scripts/validate/case-source-quality.mjs
+++ scripts/validate/case-source-quality.mjs
@@ -272,6 +272,7 @@ const primaryReferenceLinks = new Set([
   'https://mp.weixin.qq.com/s/nhiwxqy5qxj_xsxtzgpyba', // 钦州网警官方微信
   'https://mp.weixin.qq.com/s/mmawdyonqffbbdzexx-qaa', // 公安部网安局官方微信
   'https://mp.weixin.qq.com/s/-4rbqfafnygl6uz2l8mk1a', // 成都公安官方微信
+  'https://jsnews.jschina.com.cn/jczx/202502/t20250218_s67b41f81e4b04dff9907e0f5.shtml', // 江苏检察在线来源，太仓市人民检察院官网宣传看点索引指向
   'https://news.qq.com/rain/a/20250415a08y8c00', // 上海市徐汇区新闻办公室官方账号托管页
   'https://www.zhongyuan.gov.cn/rdhy/9581840.jhtml', // 政府门户转载抖音黑板报官方治理公示
   'https://x.com/alvierid/status/1999403353466421320', // 安全研究人员原始披露
