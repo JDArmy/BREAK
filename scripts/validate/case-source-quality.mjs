4ffc169d571732259ce2f5576278d1bf1aa017eb
monyer <1435145+Monyer@users.noreply.github.com>
2026-06-26 12:35:23 +0800

    继续补强案例 primary 来源

--- scripts/validate/case-source-quality.mjs
+++ scripts/validate/case-source-quality.mjs
@@ -38,6 +38,7 @@ const primaryDomainSuffixes = [
   'chinacourt.cn',
   'chinacourt.org',
   'jcy.gov.cn',
+  'jcy.gansu.gov.cn',
   'hexushui.jcy.gov.cn',
   'baoding.jcy.gov.cn',
   'jcrb.com',
@@ -64,6 +65,7 @@ const primaryDomainSuffixes = [
   'hljcourt.gov.cn',
   'bj148.org',
   'njyhfy.gov.cn',
+  'nxfy.gov.cn',
   'liulin.gov.cn',
   'sgzjfy.gov.cn',
   'scjgj.quanzhou.gov.cn',
@@ -321,6 +323,7 @@ const primaryReferenceLinks = new Set([
   'https://www.thepaper.cn/newsdetail_forward_33164885', // 云浮市中级人民法院官方澎湃号，供稿罗定法院
   'https://xinwen.bjd.com.cn/content/s6a2bfd65d5de97bd7464c3db.html', // 北京互联网法院供稿
   'https://hzsc.hangzhou.com.cn/content/content_7015476.htm', // 上城区法院案件通报
+  'https://z.hangzhou.com.cn/2022/wangan/content/content_8269934.htm', // 杭州网警以案说法，假冒 imToken 钱包盗币案
   'https://mp.weixin.qq.com/s/zm3kcgvf3bselnsmgdcglq', // 扬州经济技术开发区人民检察院官网要闻列表指向的官方微信原文
   'https://mp.weixin.qq.com/s?__biz=mzawntgwnjy0nq==&mid=2909647260&idx=1&sn=724da208d4480ad7ac2e411282b0556f', // 樊城发布政务微信
   'https://mp.weixin.qq.com/s?__biz=mzg4nta2mdu0oq==&mid=2247530131&idx=1&sn=69682338439f50044b36db5956286c8b', // 成都市市场监管政务微信
